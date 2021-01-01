5760496644dea4a365a0a3baeea9416c531e6065
SupinePandora43 <supinepandoraggg@gmail.com>
2021-01-01 14:30:40 +0500

    lua tests

--- .github/workflows/test.lua
+++ .github/workflows/test.lua
@@ -1,13 +1,44 @@
 hook.Add("Tick", "Template_CloseServer", engine.CloseServer)
 require("dotnet")
 
+local function benchmark_single(name, fn)
+	local startTime = SysTime()
+	fn()
+	local elapsed = SysTime()-startTime
+	print(name.."_single"..elapsed)
+	return elapsed
+end
+
+local function benchmark_thousand(name, fn)
+	local startTime = SysTime()
+	for i=0,1000 do
+		fn()
+	end
+	local elapsed = SysTime()-startTime
+	print(name.."_thousand"..elapsed)
+	return elapsed
+end
+
+local function benchmark(name, fn)
+	return {
+		single = benchmark_single(name, fn),
+		thousand = benchmark_thousand(name, fn)
+	}
+end
+
 local function run_test()
 	local module_loaded = dotnet.load("NativeMath")
 	assert(module_loaded)
 
 	-----------------------
 
+	print(jit.status())
 
+	local lua_math_Approach = benchmark("math_Approach", function()
+		return math.Approach(0,100,20)
+	end)
+
+	PrintTable(lua_math_Approach)
 
 	-----------------------
 
