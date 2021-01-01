baee9052cfccde393cdd2863a8bb585e73dcb983
SupinePandora43 <supinepandoraggg@gmail.com>
2021-01-01 13:55:22 +0500

    set NativeMath to nil

--- NativeMath/NativeMath.cs
+++ NativeMath/NativeMath.cs
@@ -1,29 +1,32 @@
-﻿using GmodNET.API;
+using GmodNET.API;
 using System;
 
 namespace NativeMath
 {
-    public class NativeMath : IModule
-    {
-        public string ModuleName => "NativeMath";
+	public class NativeMath : IModule
+	{
+		public string ModuleName => "NativeMath";
 
-        public string ModuleVersion => "0.0.1";
+		public string ModuleVersion => "0.0.1";
 
-        public void Load(ILua lua, bool is_serverside, ModuleAssemblyLoadContext assembly_context)
-        {
-            lua.PushSpecial(SPECIAL_TABLES.SPECIAL_GLOB);
-            lua.PushManagedFunction((lua) =>
-            {
-                Console.WriteLine("native mafs");
-                return 0;
-            });
-            lua.SetField(-2, "NativeMath");
-            lua.Pop();
-        }
+		public void Load(ILua lua, bool is_serverside, ModuleAssemblyLoadContext assembly_context)
+		{
+			lua.PushSpecial(SPECIAL_TABLES.SPECIAL_GLOB);
+			lua.PushManagedFunction((lua) =>
+			{
+				Console.WriteLine("native mafs");
+				return 0;
+			});
+			lua.SetField(-2, "NativeMath");
+			lua.Pop();
+		}
 
-        public void Unload(ILua lua)
-        {
-
-        }
-    }
+		public void Unload(ILua lua)
+		{
+			lua.PushSpecial(SPECIAL_TABLES.SPECIAL_GLOB);
+			lua.PushNil();
+			lua.SetField(-2, "NativeMath");
+			lua.Pop();
+		}
+	}
 }
