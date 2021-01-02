4c340b93cac57e6b3cc660c9241b5dbd9fe3ef04
SupinePandora43 <supinepandoraggg@gmail.com>
2021-01-02 18:42:33 +0500

    sha256 unicode test

--- .github/workflows/test.lua
+++ .github/workflows/test.lua
@@ -8,8 +8,10 @@ local function run_test()
 	-----------------------
 
 	local sha256 = HashCalc.CalculateSHA256("test")
-	print("SHA256 = " .. sha256)
 	assert(sha256==string.upper("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"))
+
+	local sha256_unicode = HashCalc.CalculateSHA256("☭")
+	assert(sha256_unicode==string.upper("d7f6fc82e3665ba40899bbf7bf6dc6f5c7f349ea054f6360c1d983f01c07601a"))
 
 	-----------------------
 
