1c2c848f014069bffab9e588af8e2020fd4f6eae
SupinePandora43 <supinepandoraggg@gmail.com>
2021-01-02 18:41:33 +0500

    fix casing

--- .github/workflows/test.lua
+++ .github/workflows/test.lua
@@ -9,7 +9,7 @@ local function run_test()
 
 	local sha256 = HashCalc.CalculateSHA256("test")
 	print("SHA256 = " .. sha256)
-	assert(sha256=="9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
+	assert(sha256==string.upper("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"))
 
 	-----------------------
 
